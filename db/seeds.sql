af58a987c309e48fade1df96ebb74d11fbf7b8f0
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-22 09:25:36 -0500

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
+
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Southwestern Veggie Burger');
+
+
+
