1d5122007e1b3a0dfc03002dfd02b8093e95947f
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-22 16:32:09 -0500

    made changes to burger file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,3 @@
+var burger = require(burger.js);
+var express = require(express);
+
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,27 @@
 var orm = require('./orm.js');
 
+var burger = {
+  selectAll: function(cb){
+      orm.selectAll('burgers', function(res){
+        cb(res);
+    });
+  },
+  insertOne: function(val, cb){
+    orm.insertOne('burgers', 'burger_name', val, function(res){
+      cb(res);
+    });
+  },
+  updateOne: function(colVal, conditionVal, cb){
+    orm.updateOne('burgers', 'devoured', colVal, 'id', conditionVal, function(res){
+      cb(res);
+    });
+  },
+  deleteOne: function(conditionVal, cb){
+    orm.deleteOne('burgers', 'id', conditionVal, function(res){
+      cb(res);
+    });
+  }
 
-
-
-
-
+};
 
 module.exports = burger;
