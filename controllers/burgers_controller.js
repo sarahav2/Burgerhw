5cb805a2d3a50c981f1a2e9667f9fcb93e725bce
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-24 09:46:59 -0500

    made changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,11 +9,11 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
+// router.get("/", function(req, res) {
+//   res.redirect("/burgers");
+// });
 
-router.get("/burgers", function(req, res) {
+router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
       burgers: data
